5e190aad43d95d3f8c65d9c44b26ff93d2992337
Victor B <vicbab@yandex.com>
2021-11-16 16:28:26 -0500

    initial position

---
+++ life.py
@@ -0,0 +1,62 @@
+import math, random
+
+def minimax():
+
+def senseOfTime():
+    # The thread must update at every tick. Here, make the constance hypothesis lie and let it be.
+
+class World:
+    def __init__(self, facts, space, time):
+        self.facts = facts
+
+class Situation:
+    def __init__(self, position, orientation):
+        self.position = position
+        self.orientation = orientation
+
+class Memory(Tuple):
+    def __init__(self):
+
+
+class Player:
+# the Player class is the Dasein class, the lived-body class, the qualia wielding res extensa.
+   def __init__(self, memory, field, projection):
+       self.memory = memory # sense of past
+       self.field = field # sense of now
+       self.projection = projection # sense of the possible
+
+   def getField(self):
+       print(self.field)
+
+   def project(self):
+       field = self.field
+       self.memory.
+
+
+
+
+
+def world():
+    # space, time, randomness
+
+
+possibleMoves = [][]
+moves = [1, 2, 3, 4]
+past = [][]
+now =
+
+# there must always be a trifold structure of (game) temporality:
+# 1/ past (the n-D determined set of played moves)
+# 2/ now (nothing(ness), the current situation -- meaning the _stance_ in which the player [first-hand, embodied, lived perspective], the junction of past and futures (accounting for the possible and the likely))
+# 3/ future (derivated from the "Many"ness of the possible worlds... Butterfly effect--Chaos theory. From within the Chaos of the possible arises the Realized, meaning the "what did(would, has, had, AND **WILL** (most probably) happen)")
+#       3/1/ This. This is the most complex. The à-venir is yet to come, à-venir, literally, the "yet-to-come". The Probabilistic is extramathematical and must be treated as such. Therefore, an algorithmic in-carn-ation of life(hear "game")world must be:
+#               3/1/1/ Embodied
+#               3/1/2/ In-the-world
+#               3/1/3/ Immediate (almost intuitive-like, as immediate as computational resources can be.)
+# the decision arises from a multifactorial situational/situated/lived-as-such/qualitative (qualia-of-the-experience-at-(first-)hand). as such, "free-"will is the _result_ of past, joined with the possible (i.e. the futureS (possible worlds))
+# the decision can never be un-emotional, but it HAS to be in order to let objectivity occur. thus, objectivity is non-occuring.
+# Difficulty: accounting for the rhizomatic non-thetic and non-arborescent (obviously) sense of time consciousness
+
+# intentional consciousness.
+field = future
+player = Player()
